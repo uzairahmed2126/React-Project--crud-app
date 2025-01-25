94fab892da87abd9a0aa930f26f9625d1111ad68
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-01-26 01:44:09 +0530

    .

--- crud-app/src/components/Inputs/index.jsx
+++ crud-app/src/components/Inputs/index.jsx
@@ -1,21 +1,49 @@
+import { useEffect, useState } from "react";
 import InputData from "../InputsData";
 function Inputs() {
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("sdf");
-  }
   return (
     <div>
-      <form id="hey" onSubmit={handleSubmit}>
-        <input id="employee-name" type="text" placeholder="Employee Name" />
-        <input id="employee-age" type="number" placeholder="Employee Age" />
+      <form id="hey">
+        <input
+          id="employee-name"
+          type="text"
+          placeholder="Employee Name"
+          // value={formData.empName}
+          // onChange={handleInputChange}
+        />
+        <input
+          id="employee-age"
+          type="number"
+          placeholder="Employee Age"
+          // value={formData.empAge}
+          // onChange={handleInputChange}
+        />
         <input
           id="employee-position"
           type="text"
           placeholder="Employee Position"
+          // value={formData.empPosition}
+          // onChange={handleInputChange}
         />
         <button type="submit">Add Item</button>
       </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>age</th>
+            <th>position</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* <td>{formData.empName}</td>
+            <td>{formData.empAge}</td>
+            <td>{formData.empPosition}</td> */}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
